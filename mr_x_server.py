de6ecb10a87245c92c66838a05198aa58abaf60a
SaritaAgarwal15 <47062862+SaritaAgarwal15@users.noreply.github.com>
2019-10-26 01:12:55 +0530

    Update mr_x_server.py

--- mr_x_server.py
+++ mr_x_server.py
@@ -9,6 +9,7 @@ import threading
 import gui
 
 class Window(QMainWindow, gui.Ui_MainWindow):
+    #PyQT main window widget for all components
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
         self.lis = rules.start()
